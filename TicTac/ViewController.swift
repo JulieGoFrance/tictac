6fb16a6e64998fdb08f1302140c62cddb06971ed
Julie Go <julie.gosselin84@yahoo.fr>
2022-07-16 16:50:00 +0200

    debut de la refactorisation des fonctions

--- TicTac/ViewController.swift
+++ TicTac/ViewController.swift
@@ -33,14 +33,109 @@ class ViewController: UIViewController {
     var tourEnCours = Tour.Croix
     let ROND = "X"
     let CROIX = "O"
+    var plateau = [UIButton]()
+    var scoreRond = 0
+    var scoreCroix = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    func initialiserPlateau(){
+        plateau.append(a1)
+        plateau.append(a2)
+        plateau.append(a3)
+        plateau.append(b1)
+        plateau.append(b2)
+        plateau.append(b3)
+        plateau.append(c1)
+        plateau.append(c2)
+        plateau.append(c3)
+        
+    }
+   
 
     @IBAction func boardTapAction(_  sender: UIButton) {
+        
+        ajouterAuPlateau(sender)
+        if verifierVictoire(CROIX){
+            afficherResultats(titre: "Joueur 1 a gagné")
+            scoreRond += 1
+        }
+        if verifierVictoire(ROND){
+            afficherResultats(titre: "Joueur 2 a gagné")
+            scoreCroix += 1
+        }
+        if (plateauPlein()){
+            afficherResultats(titre: "Commencer")
+        }
+    
          
+    }
+    
+    func verifierVictoire(_ s : String)-> Bool {
+        
+        // victoire Horizontales
+        if thisSymbol(a1, s) && thisSymbol(a2, s)  && thisSymbol(a3, s){
+            return true
+        }
+        if thisSymbol(b1, s) && thisSymbol(b2, s)  && thisSymbol(b3, s){
+            return true
+        }
+        if thisSymbol(c1, s) && thisSymbol(c2, s)  && thisSymbol(c3, s){
+            return true
+        }
+        
+        // victoires verticales
+        
+        if thisSymbol(a1, s) && thisSymbol(b1, s)  && thisSymbol(c1, s){
+            return true
+        }
+        if thisSymbol(a2, s) && thisSymbol(b2, s)  && thisSymbol(c2, s){
+            return true
+        }
+        if thisSymbol(a3, s) && thisSymbol(b3, s)  && thisSymbol(c3, s){
+            return true
+        }
+        // victoires diagonales
+        
+        if thisSymbol(a1, s) && thisSymbol(b2, s)  && thisSymbol(c3, s){
+            return true
+        }
+        if thisSymbol(a3, s) && thisSymbol(b2, s)  && thisSymbol(c1, s){
+            return true
+        }
+        
+        return false
+    }
+    func thisSymbol(_ button : UIButton, _ symbol : String) -> Bool{
+        return button.title(for: .normal) == symbol
+    }
+    
+    func plateauPlein() -> Bool {
+      
+        for button in plateau {
+            if button.title(for: .normal) == nil{
+                return false
+            }
+            
+        }
+       return true
+    }
+    func afficherResultats(titre : String){
+        let message = "\n1 jouer " + String(scoreCroix) + "\n2 joueur " + String(scoreRond)
+        let ac = UIAlertController(title: titre, message: message, preferredStyle: .actionSheet)
+        ac.addAction(UIAlertAction(title: "Recommencer", style: .default, handler: {
+            (_)in self.ReinitPlateau()
+            
+        }))
+        self.present(ac, animated: true)
+    }
+    func ReinitPlateau() {
+        for button in plateau {
+            button.setTitle(nil, for: .normal)
+            button.isEnabled = true
+        }
     }
     func ajouterAuPlateau(_  sender: UIButton){
         if (sender.title(for: .normal) == nil) {
@@ -51,12 +146,13 @@ class ViewController: UIViewController {
                 turnLabel.text = CROIX
                 
             }
-            if (tourEnCours == Tour.Croix) {
+            else if (tourEnCours == Tour.Croix) {
                 sender.setTitle(CROIX, for: .normal)
                 tourEnCours = Tour.Rond
                 turnLabel.text = ROND
                 
             }
+            sender.isEnabled = false
         }
     }
     
